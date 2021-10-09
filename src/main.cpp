9bb16671161e313ef3f5dbca74b3f4924aa8357b
Dmitry Kalinin <x.shreddered.x@gmail.com>
2021-10-10 00:53:00 +0300

    added support for 'token-file' command line argument via boost::program_options

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
-#include <iostream>
-#include <boost/asio.hpp>
+#include "coin/miner.hpp"
+#include "coin/math/eval.hpp"
 
-#include "http/session.hpp"
+#include <nlohmann/json.hpp>
+
+#include <boost/program_options.hpp>
+
+#include <fstream>
+#include <iostream>
+
+using nlohmann::json;
 
 int main(int argc, char* argv[]) {
-    http::Session session;
-    std::cout << session.get("https://google.com") << std::endl;
-    std::cout << session.get("https://ya.ru") << std::endl;
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "print this help message")
+        ("token-file", boost::program_options::value<std::string>(),
+            "path to file with token");
+    boost::program_options::variables_map vm;
+    boost::program_options::store(
+            boost::program_options::parse_command_line(argc, argv, desc),
+            vm);
+    boost::program_options::notify(vm);
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+    if (!vm.count("token-file")) {
+        std::cerr << desc << std::endl;
+        return 1;
+    }
+    std::ifstream in{vm["token-file"].as<std::string>()};
+    std::string token;
+    std::getline(in, token);
+    coin::Miner miner{token};
+    miner.run();
     return 0;
 }
