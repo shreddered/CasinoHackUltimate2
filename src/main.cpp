8634bbe06e1a4677fb732c97bea89311d8bdee57
Dmitry Kalinin <x.shreddered.x@gmail.com>
2021-10-10 20:53:26 +0300

    token is now obtained from environmental variable

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,14 @@
 
 #include <boost/program_options.hpp>
 
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 
 using nlohmann::json;
 
 int main(int argc, char* argv[]) {
-    boost::program_options::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "print this help message")
-        ("token", boost::program_options::value<std::string>(),
-            "VK API access token");
-    boost::program_options::variables_map vm;
-    boost::program_options::store(
-            boost::program_options::parse_command_line(argc, argv, desc),
-            vm);
-    boost::program_options::notify(vm);
-    if (vm.count("help")) {
-        std::cout << desc << std::endl;
-        return 0;
-    }
-    if (!vm.count("token")) {
-        std::cerr << desc << std::endl;
-        return 1;
-    }
-    std::string token = vm["token"].as<std::string>();
+    std::string token = std::getenv("TOKEN");
     coin::Miner miner{token};
     miner.run();
     return 0;
